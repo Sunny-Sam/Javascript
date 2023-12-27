abfb92a05e0853bb36db3611dbfddf2902471b96
Decoder-sam <sunny.sharma@easyeat.ai>
2023-12-27 15:34:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -27,8 +27,9 @@ cars = [
   { make: "Benz", model: "C-class", year: 2019 },
   { make: "RR", model: "Ghost", year: 2023 },
 ];
+console.log(cars);
 cars.shift();
+console.log(cars);
 cars.push({ make: "Honda", model: "Civic", year: 2020 });
 cars[1].model = "Accord";
-
 console.log(cars);
